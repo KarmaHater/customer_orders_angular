019402117d0d0c537508fcc6998590663bd853c8
Adnard-kod <andra.lally@gmail.com>
2015-08-18 15:02:45 +0100

    can view customer orders

--- js/controllers/customer-orders-controller.js
+++ js/controllers/customer-orders-controller.js
@@ -1,3 +1,23 @@
 angular.module('CustomerApp')
 .controller('CustomerOrdersCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+  $scope.customer = null;
+  $http.get('users.json').success(function(data){
+    for (var i = data.length - 1; i >= 0; i--) {
+      if (data[i].id === $routeParams.id) {
+        $scope.customer = data[i]
+        $scope.getCutomerOrders(data[i].id)
+        break;
+      };
+    };
+  });
+  $scope.getCutomerOrders = function(id) {
+      $http.get('orders.json').success(function(data){
+      for (var i = data.length - 1; i >= 0; i--) {
+        if (data[i].customer_id === $routeParams.id) {
+          $scope.customerOrders = data[i].orders
+          break;
+        };
+      };
+    });
+  }
 }]);
