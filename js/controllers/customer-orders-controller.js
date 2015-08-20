2ef91fea99f83763612daec184c2e6c3a2a61c3d
Adnard-kod <andra.lally@gmail.com>
2015-08-20 13:42:37 +0100

    refactor to use customer facory

--- js/controllers/customer-orders-controller.js
+++ js/controllers/customer-orders-controller.js
@@ -1,13 +1,22 @@
 angular.module('CustomerApp')
-.controller('CustomerOrdersCtrl', ['$scope', '$http', '$routeParams', '$log', 'ordersFactory', function ($scope, $http, $routeParams, $log, ordersFactory) {
-  $http.get('users.json').success(function(data){
-    _.filter(data, function(cust){ 
-      if (cust.id === $routeParams.id) {
-        $scope.customer = cust
-        $scope.getCutomerOrders(cust.id)
-      };
+.controller('CustomerOrdersCtrl', ['$scope', '$http', '$routeParams', '$log', 'ordersFactory', 'customersFactory', function ($scope, $http, $routeParams, $log, ordersFactory, customersFactory) {
+  function init() {
+    customersFactory.getCustomers()
+    .success(function(data){
+      _.filter(data, function(cust){ 
+        if (cust.id === $routeParams.id) {
+          $scope.customer = cust
+          $scope.getCutomerOrders(cust.id)
+        };
+      });
+    })
+    .error(function(data, status, headers, config) {
+      $log.log("data: " + data)
+      $log.log("status: " + status)
+      $log.log("config: " + config)
     });
-  });
+  };
+  init();
   $scope.getCutomerOrders = function(id) {
     ordersFactory.getOrders()
     .success(function(data){
