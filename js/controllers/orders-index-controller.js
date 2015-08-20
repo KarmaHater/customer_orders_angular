e05f379296e91cf2ec4e6bac6ecfbbe03f6346ac
Adnard-kod <andra.lally@gmail.com>
2015-08-20 15:43:50 +0100

    add factory for filters

--- js/controllers/orders-index-controller.js
+++ js/controllers/orders-index-controller.js
@@ -1,17 +1,19 @@
 angular.module('CustomerApp')
-.controller('OrdersIndexCtrl', ['$scope', '$log', 'ordersFactory', function ($scope, $http, $log, ordersFactory) {
+.controller('OrdersIndexCtrl', ['$scope', '$log', 'ordersFactory', function ($scope, $log, ordersFactory) {
+  $scope.orders = [];
   function init(){
-    debugger
     ordersFactory.getOrders()
-    // .success(function(data){
-    //   $scope.orders = data
-    //   debugger
-    // })
-    // .error(function(data, status, headers, config) {
-    //   $log.log("data: " + data)
-    //   $log.log("status: " + status)
-    //   $log.log("config: " + config)
-    // });
+    .success(function(data){
+      _.each(data, function(o) {
+        $scope.orders.push(o.orders)
+      });
+      $scope.orders = _.flatten($scope.orders);
+    })
+    .error(function(data, status, headers, config) {
+      $log.log("data: " + data)
+      $log.log("status: " + status)
+      $log.log("config: " + config)
+    });
   }
   init();
 }])
